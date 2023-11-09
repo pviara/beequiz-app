abca2ab9efa4e80d2284450bd329becdde8d6363
Pierre Viara <pierre.viara@outlook.com>
2023-11-09 19:56:36 +0100

    feat(landing): add headbar

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'landing',
     templateUrl: './landing.component.html',
+    styleUrls: ['./landing.component.scss'],
 })
 export class LandingComponent {}
